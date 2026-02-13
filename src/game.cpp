7270a38968d569bc22210d4f8c73c3daecbf5ce0
DESBOIS-RENAUDIN Nils <nils.desbois-renaudin@grenoble-inp.org>
2026-02-13 10:58:08 +0100

    adding assets for publication

--- src/game.cpp
+++ src/game.cpp
@@ -63,7 +63,7 @@ game_t* game_init( void ) {
 
     world_flags_t world_flags;
     set_default_world_flags( &world_flags );
-    world_flags.special_seed = FLAT;  // FLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT
+    // world_flags.special_seed = FLAT;  // FLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT
     game->world = create_world( &world_flags );
     if ( game->world == NULL ) {
         perror("Failed to create world");
